f9b6541b4fc71af39ed46f0d7b3e98afd141cf9c
Natalie Devenbeck <devenbeck@usf.edu>
2025-03-30 16:27:44 -0400

    Add fetchProductsAsync using async/await and try/catch

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,4 +1,4 @@
-//Fetch products with .then()
+//Task 2: Fetch products with .then()
 ////Create function fetchProductsThen that uses fetch() and .then() to get product data from url
 const BASE_URL = "https://www.course-api.com/javascript-store-products";
 function fetchProductsThen() {
@@ -18,4 +18,17 @@ function fetchProductsThen() {
         console.error('Fetch failed:', error.message); //logs error message to console
         throw error;
     });
+};
+//Task 3: Fetch products with async/await
+async function fetchProductsAsync() {    //to try to fetch product data
+    try { 
+        const response = await fetch(BASE_URL); 
+        if (!response.ok) { //check response
+            throw new Error(`Failed to fetch data: ${response.status}`); //Logs error message to console
+        }
+        const products = await response.json(); //Return response in JSON format
+        displayProducts(products); //Calls helper function to render products to the page
+    } catch (error) {
+        handleError(error); //if error occurs, passes the errors to handleError function
+    }
 };
